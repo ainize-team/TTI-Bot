0227bdbba5c755edfdc0ad49be21be4ecd599dd6
김영재 <byeongal@comcom.ai>
2022-12-08 12:36:49 +0900

    add stable-diffusion-v2-1

--- src/enums.py
+++ src/enums.py
@@ -27,6 +27,7 @@ class ModelEnum(StrEnum):
     STABLE_DIFFUSION_V1_4 = "stable-diffusion-v1-4"
     STABLE_DIFFUSION_V1_5 = "stable-diffusion-v1-5"
     STABLE_DIFFUSION_V2 = "stable-diffusion-v2"
+    STABLE_DIFFUSION_V2_1 = "stable-diffusion-v2-1"
 
 
 class ErrorMessage(StrEnum):
